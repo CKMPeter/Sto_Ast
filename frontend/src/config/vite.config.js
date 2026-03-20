b580e920632155cf1bc53a6922ab78d9337d441b
DoomDayKross <21110777@student.hcmute.edu.vn>
2026-03-20 14:39:59 +0700

    Update vite.config.js

--- frontend/src/config/vite.config.js
+++ frontend/src/config/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
+    https: false/*{
       cert: './src/cert/cert.pem',
       key: './src/cert/key.pem',
     },
@@ -14,6 +14,6 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
-    },
+    },*/
   },
 });
